913501044ecf3ccea36da13c0b2079bb6ac2d4a2
ANgajasinghe <angajasinghe@gmail.com>
2021-06-03 12:48:10 +0530

    [Routing] Add navigation

--- Source/nextjs-app/pages/index.js
+++ Source/nextjs-app/pages/index.js
@@ -1,11 +1,22 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function HomePage() {
   return (
     <div className={styles.container}>
      <h1> Hello next world  </h1>
+
+    <ul>
+
+      <li>
+        <Link href="/portfolio" >Portfolio</Link>
+      </li>
+
+    </ul>
+
+
     </div>
   )
 }
