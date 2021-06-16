e14aa3d1a3a5ecadca6584261c097ddfc10051bc
ANgajasinghe <angajasinghe@gmail.com>
2021-06-16 13:23:05 +0530

    [Page Pre-Rendering] IRS

--- 5.Page-Pre-Rendering/page-pre-rendering-code/pages/index.tsx
+++ 5.Page-Pre-Rendering/page-pre-rendering-code/pages/index.tsx
@@ -26,18 +26,29 @@ export default function HomePage(props: any) {
 // this will executed first.
 // this function never executed at the browser.
 // this function has server side capability.
+// this will execute with build page
 export async function getStaticProps() {
 
     // here we can add server side codes rg:- file-system
     // process.cwd() -> give current working directory, this will be the root
+
+    // this is only for static pre-rendering only we cannot update data here when build
     const filePath = path.join(process.cwd(),'data','dummy-backend.json');
     let jsonData = fs.readFileSync(filePath).toString();
 
+    // SO CAN"T WE UPDATE DATA ??
+    // next.js offer will offer another way -?> Incremental Static Generation
+    // this will iterative process process
     console.log(jsonData);
     return {
         props: {
             products: JSON.parse(jsonData).products
-        }
+        },
+        // this static generation will regenerate with 120 seconds.
+        // this will help full in the production (IRS)
+        revalidate: 120
     }
 }
+
+
 
