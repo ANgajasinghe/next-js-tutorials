17e49e3f9d9d04d8fb8ca6591b0fb4d9a63fb4f3
ANgajasinghe <angajasinghe@gmail.com>
2021-06-30 00:48:36 +0530

    Event app updated

--- 7. Optimizing NextJS Apps/event-app/pages/index.js
+++ 7. Optimizing NextJS Apps/event-app/pages/index.js
@@ -1,4 +1,5 @@
-import  Head  from 'next/head';
+import Head from 'next/head';
+
 import { getFeaturedEvents } from '../helpers/api-util';
 import EventList from '../components/events/event-list';
 
@@ -6,10 +7,12 @@ function HomePage(props) {
   return (
     <div>
       <Head>
-        <title> Next.js Events </title>
-        <meta name='description' content='this is a description'> </meta>
+        <title>NextJS Events</title>
+        <meta
+          name='description'
+          content='Find a lot of great events that allow you to evolve...'
+        />
       </Head>
-      
       <EventList items={props.events} />
     </div>
   );
@@ -20,10 +23,10 @@ export async function getStaticProps() {
 
   return {
     props: {
-      events: featuredEvents
+      events: featuredEvents,
     },
-    revalidate: 1800
-  }
+    revalidate: 1800,
+  };
 }
 
 export default HomePage;
